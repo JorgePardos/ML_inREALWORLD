66c42420e5bd857b00de06defacdebe08c6bdcde
JorgePardos <113998329+JorgePardos@users.noreply.github.com>
2022-09-20 16:20:58 +0200

    Update 1_main_page.py

--- 1_main_page.py
+++ 1_main_page.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.title("Buenas Tardes")
 
-my_button = st.button("PINCHE AQUÍ")
+
 if st.button('PINCHE AQUI'):
-  st.ballons()
+  st.balloons()
   st.write('FELICIDADES')
 
 
